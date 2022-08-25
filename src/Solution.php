9a8d63f2b4d0efb09ecfa8a45967ba3645bbd4c1
oji <oji@riau.go.id>
2022-08-25 16:33:07 +0700

    bug fixes, add response

--- src/Solution.php
+++ src/Solution.php
@@ -4,14 +4,20 @@ namespace OjiSatriani\Attendance;
 
 class Solution
 {
-    protected static $ipAddress;
-    protected static $port;
-    protected static $key;
+    protected $ipAddress;
+    protected $port;
+    protected $key;
+    protected $ping;
+    protected $dateStart;
+    protected $dateEnd;
 
     public function __construct(array $config = []) {
         $this->ipAddress    = $config['ip_address'] ?? '';
         $this->port         = $config['port'] ?? '';
         $this->key          = $config['key'] ?? 0;
+        $this->dateStart    = $config['date_start'] ?? NULL;
+        $this->dateEnd      = $config['date_end'] ?? date('Y-m-d H:i:s');
+        $this->pin          = $config['pin'] ?? 'All';
     }
 
     public static function init($config = NULL)
@@ -19,7 +25,7 @@ class Solution
         return new static($config);
     }
 
-    private function parse($data,$tag_opening,$tag_closing){
+    public function parse($data,$tag_opening,$tag_closing){
         $data               = " " . $data;
         $result             = "";
         $start              = strpos($data, $tag_opening);
@@ -31,38 +37,58 @@ class Solution
                 $result     = substr($data, $start+strlen($tag_opening), $end-strlen($tag_opening));
             }
         }
-        return $result;    
+        return $result;
     }
 
     public function connect()
     {
-        return is_resource(@fsockopen($this->ipAddress, $this->port, $errno, $errstr, 1)) ? true : false;
+        return @fsockopen($this->ipAddress, $this->port, $errno, $errstr, 1);
     }
 
     public function getAttLog()
     {
-        if ($this->connect())
-        {
-            $soap_request   = "<GetAttLog><ArgComKey xsi:type=\"xsd:integer\">".$this->key."</ArgComKey><Arg><PIN xsi:type=\"xsd:integer\">All</PIN></Arg></GetAttLog>";
-            $newLine        = "\r\n";
-            fputs($konek, "POST /iWsService HTTP/1.0". $newLine);
-            fputs($konek, "Content-Type: text/xml". $newLine);
-            fputs($konek, "Content-Length: ". strlen($soap_request) . $newLine . $newLine);
-            fputs($konek, $soap_request . $newLine);
-            $buffer="";
-            while($Response=fgets($konek, 1024)){
-                    $buffer=$buffer.$Response;
+        try {
+            $connect            = $this->connect();
+            if (is_resource($connect))
+            {
+                $soap_request   = "<GetAttLog><ArgComKey xsi:type=\"xsd:integer\">".$this->key."</ArgComKey><Arg><PIN xsi:type=\"xsd:integer\">".$this->pin."</PIN></Arg></GetAttLog>";
+                $newLine        = "\r\n";
+                fputs($connect, "POST /iWsService HTTP/1.0". $newLine);
+                fputs($connect, "Content-Type: text/xml". $newLine);
+                fputs($connect, "Content-Length: ". strlen($soap_request) . $newLine . $newLine);
+                fputs($connect, $soap_request . $newLine);
+                $buffer         = "";
+                while($Response = fgets($connect, 1024)){
+                        $buffer = $buffer.$Response;
+                }
+                $log_response   = $this->parse($buffer, "<GetAttLogResponse>", "</GetAttLogResponse>");
+                $log_response   = str_replace("\r\n", "\n", $log_response);
+                $log            = explode("\n", $log_response);
+                krsort($log);
+                return $log;
+            } else {
+                return [];
             }
-            $log_response   = $this->parse($buffer, "<GetAttLogResponse>", "</GetAttLogResponse>");
-            $log_response   = str_replace("\r\n", "\n", $log_response);
-            $a_log_response = explode("\n", $log_response);
-            krsort($a_log_response);
-            $xml            = simplexml_load_string($a_log_response);
-            $json           = json_encode($xml);
-            $array          = json_decode($json, TRUE);
-            return $array;
-        } else {
-            return NULL;
+        } catch (\Throwable $th) {
+            throw $th;
         }
+    }
+
+    public function response()
+    {
+        $data           = [];
+        foreach($this->getAttLog() as $log){
+            $row        = $this->parse($log,"<Row>","</Row>");
+            $pin        = (int)$this->parse($row,"<PIN>","</PIN>");
+            $datetime   = $this->parse($row,"<DateTime>","</DateTime>");
+            if ($pin)
+            {
+                $data[] = [
+                                'pin'       => $pin,
+                                'datetime'  => $datetime,
+                ];
+            }
+        }
+        return $data;
     }
 }
